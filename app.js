bb4a593a822a8b60d7f98b687066c50497f2419e
Saúl Camacho <electrohead1200@gmail.com>
2023-11-17 18:22:15 -0600

    Mostrando datos de una API REST en la CLI de Node.js

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ import chalk from "chalk";
 
 const API_KEY = "6852c0d482136b98d9f15d4a5380de21";
 
+function displayWeather(city, weatherData) {
+    console.log(chalk.yellow(`\nInformación del Clima: ${city}`));
+    console.log(chalk.cyan("Descripcion: "), weatherData.weather[0].description);
+    console.log(chalk.cyan("Temperatura: "), `${weatherData.main.temp} °C`);
+    console.log(chalk.cyan("Humedad: "), `${weatherData.main.humidity} %`);
+    console.log(chalk.cyan("Velocidad del Viento: "), `${weatherData.wind.speed} m/s`);
+}
+
+function handleError(err) {
+    console.log(chalk.red("Error: "), err.message);
+    process.exit(1);
+}
+
 async function getWeather(city) {
     try {
         let endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
@@ -14,10 +27,11 @@ async function getWeather(city) {
                 units: "metric"
             }
         });
-        console.log(response);
+        // console.log(response);
         return response.data;
     } catch (error) {
         console.log(chalk.red(error));
+        throw new Error('No es posible obtener Datos')
     }
 }
 
@@ -27,9 +41,11 @@ function getData() {
     if (!city) {
         console.log(chalk.red("Ingresa el nombre de la ciudad"));
         console.log(chalk.red("Ejecuta el siguiente comando node app.js [nombre ciudad]"));
-    };
+    }
 
-    getWeather(city);
+    getWeather(city)
+        .then((weatherData) => displayWeather(city, weatherData))
+        .catch(handleError)
 }
 
 getData();
